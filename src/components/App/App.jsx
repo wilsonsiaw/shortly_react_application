a56b2427d4ea9c52d96e634363b6d520b5de0245
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-01 14:25:38 +0200

    🦄 refactor(Application): Import Header
    The Header component is imported into the the root component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import Header from '../Header/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
