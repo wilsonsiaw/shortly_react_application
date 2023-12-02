9b75fe997bcb2c09df4eb4b0cab89f3b7192f09d
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-02 10:28:40 +0200

    ✨ feat(Body Component): Create Body
    A body component is created and  the content is put in it.

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Body.css';
+
+const Body = () => {
+  return (
+    <div className='container'>
+      <div className="body-wrapper">
+          <div className="heading">
+              <h3>Advanced Statistics</h3>
+          </div>
+          <div className="para">
+              <p>Track how your links are performing across the web with our advanced statistics dashboard</p>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Body
