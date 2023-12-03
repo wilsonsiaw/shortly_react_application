39d7d3e4cad85367f1aa5bdac66cd0ad4bc04cfe
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-03 09:50:44 +0200

    ✨ feat(Started Component): Add Content
    Add the content of the Started component.

---
+++ src/components/Started/Started.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Started.css';
+import background from '../../assets/images/bg-shorten-desktop.svg';
+
+const Started = () => {
+  return (
+    <div className='bgWrapper'>
+        <div className="bgContent">
+            <div className="bgHeading">
+                <h3>Boost your links today</h3>
+            </div>
+            <div className="bgBtn">
+                <button>Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Started
