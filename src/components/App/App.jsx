407a85e47ca8a42d42e91ecb0bbf4f8875c0668c
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-03 09:48:02 +0200

    ✨ feat(App Component): Add Started
    Add the Started component to the root component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from '../Header/Header';
 import Hero from '../Hero/Hero';
 import Body from '../Body/Body';
+import Started from '../Started/Started';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Hero />
       <Body />
+      <Started />
     </div>
   )
 }
