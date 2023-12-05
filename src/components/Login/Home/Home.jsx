7ce3d02eeebd8e8fa3084633b228757ad252dfe4
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-05 08:03:53 +0200

    ✨ feat(Home Component): Create Home
    Create a Home component that will house all the other components for the Home page.

---
+++ src/components/Login/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../Header/Header';
+import Hero from '../../Hero/Hero';
+import Body from '../../Body/Body';
+import Started from '../../Started/Started';
+import Footer from '../../Footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Body />
+      <Started />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
