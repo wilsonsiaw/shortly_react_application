82fc924b2efbf410316eac0db683445eb277c2f3
Wilson Siaw <wilsonsiawse@gmail.com>
2023-12-05 08:07:27 +0200

    🦄 refactor(App Component): Refactor App
    The App component is re-worked to house only the Routes for linking different pages.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,17 +5,23 @@ import Hero from '../Hero/Hero';
 import Body from '../Body/Body';
 import Started from '../Started/Started';
 import Footer from '../Footer/Footer';
-
+import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
+import Password from '../Password/Password';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Login/Home/Home';
 
 function App() {
 
   return (
     <div>
-      <Header />
-      <Hero />
-      <Body />
-      <Started />
-      <Footer />
+      <Routes>
+        <Route index path='/' element={<Home />}/>
+        <Route path='Signup' element={<Signup />}/>
+        <Route path='Login' element={<Login />}/>
+        <Route path='Signup' element={<Signup />}/>
+        <Route path='Password' element={<Password />}/>
+      </Routes>
     </div>
   )
 }
